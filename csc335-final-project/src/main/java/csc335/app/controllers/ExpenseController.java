48fe0481268bd30248418206a3e8ed9e90729150
Lauren Schroeder <97125605+flamberdoodle@users.noreply.github.com>
2024-12-02 15:30:17 -0700

    small changes

--- csc335-final-project/src/main/java/csc335/app/controllers/ExpenseController.java
+++ csc335-final-project/src/main/java/csc335/app/controllers/ExpenseController.java
@@ -9,7 +9,12 @@ package csc335.app.controllers;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
+import scala.collection.immutable.List;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -18,9 +23,25 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Pattern;
 
+import javax.faces.event.ActionEvent;
+
+import java.util.ArrayList;
+//import java.util.List;
+import csc335.app.UserAuth;
+
+
 public class ExpenseController {
     @FXML
     private AnchorPane contentArea;
+    private static final String USER_DATA_DIRECTORY = "data/users";
+    private FlowPane mainPane, importPane, addNewPane, editPane, removePane;
+    private Scene mainScreen, importFile, addNewExpense, editExpense, removeExpense, dateFrom, dateTo, progressBar;
+    private Button importFileButton, addNewExpenseButton, editExpenseButton, removeExpenseButton, dateFromButton, dateToButton, progressBarButton;
+    private Set<String> expenseCategories = new HashSet<>();
+    private List<Expense> expenses = new ArrayList<>();
+    private double totalBudget = 0.0;
+    private double totalExpenses = 0.0;
+    private String username;
 
     /**
      * 
@@ -32,6 +53,10 @@ public class ExpenseController {
                 Pane view = FXMLLoader.load(getClass().getResource(fxmlPath));
                 contentArea.getChildren().clear();
                 contentArea.getChildren().add(view);
+                Pane importFile = new Pane("importFile");
+                importFile.setOnMouseClicked((MouseEvent click)->{
+                    importFileClick(click, ActionEvent);
+                });
             } else {
                 System.err.println("contentArea is null. Check FXML and Controller binding.");
             }
@@ -39,12 +64,24 @@ public class ExpenseController {
             e.printStackTrace();
         }
     }
-
-    private void saveData(){
-        
+    /**
+     * 
+     * @param expenses
+     * @param totalBudget
+     * @param totalExpenses
+     */
+    private void saveData(String username, List expenses, String totalBudget, String totalExpenses) {
+        File userFile = new File(USER_DATA_DIRECTORY, username + ".txt");
+        try (FileWriter writer = new FileWriter(userFile, true)) {
+            writer.write("Expenses: " + expenses + "\n");
+            writer.write("Total Budget: " + totalBudget + "\n");
+            writer.write("Total Expenses: " + totalExpenses + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void importFileClick(){
+    private void importFileClick(ActionEvent click, ActionEvent fileEnter){
 
     }
 
